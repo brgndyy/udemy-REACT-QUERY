502e8663506784a6f707ddfb9f06c5b620b54b5b
brgndy <brgndy96@gmail.com>
2024-03-17 14:38:23 +0900

    feat: 2강 react-query-dev-tools

--- base-blog-em/src/api.js
+++ base-blog-em/src/api.js
@@ -2,7 +2,6 @@ export async function fetchPosts(pageNum = 1) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
-  throw new Error("에러입니다");
   return response.json();
 }
 
